c7c0274bff56de6cd1e6aaf51e04e42120239a4f
Yafim Vainilovich <yafimsva@gmail.com>
2020-04-03 00:28:24 -0700

    implement code that pulls in our shop data form firestore and stores it inot the shop reducer from shop page. Also icluded withSpinner HOC

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { updateCollections } from '../../redux/shop/shop.actions';
+
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
 import CollectionOverview from '../../components/collection-overview/collection-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ match }) => {
-	console.log(match);
+import {
+	firestore,
+	convertCollectionsSnapshotToMap
+} from '../../firebase/firebase.utils';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
+const ShopPage = ({ match, updateCollections }) => {
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const collectionRef = firestore.collection('collections');
+
+		collectionRef.get().then(snapshot => {
+			const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+			updateCollections(collectionMap);
+			setLoading(false);
+		});
+	});
+
 	return (
 		<div className="shop-page">
-			<Route exact path={`${match.path}`} component={CollectionOverview} />
-			<Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+			<Route
+				exact
+				path={`${match.path}`}
+				render={props => (
+					<CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+				)}
+			/>
+			<Route
+				path={`${match.path}/:collectionId`}
+				render={props => (
+					<CollectionPageWithSpinner isLoading={loading} {...props} />
+				)}
+			/>
 		</div>
 	);
 };
 
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+	updateCollections: collectionsMap =>
+		dispatch(updateCollections(collectionsMap))
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,13 +7,13 @@ export const selectCollections = createSelector(
 	shop => shop.collections
 );
 
-export const selectCollection = collectionUrlParam =>
-	createSelector(
-		[selectCollections],
-		collections => collections[collectionUrlParam]
-	);
-
 export const selectCollectionsForPreview = createSelector(
 	[selectCollections],
-	collections => Object.keys(collections).map(key => collections[key])
+	collections =>
+		collections ? Object.keys(collections).map(key => collections[key]) : []
 );
+
+export const selectCollection = collectionUrlParam =>
+	createSelector([selectCollections], collections =>
+		collections ? collections[collectionUrlParam] : null
+	);
