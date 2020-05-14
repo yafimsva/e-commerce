b1b1f57334ac13d5d92ced433a03ff0ccb8bdc94
Yafim Vainilovich <yafimsva@gmail.com>
2020-05-13 19:18:11 -0700

    Moved front end to a subdirectory, created backend with express for stripe

--- client/src/pages/collection/collection.component.jsx
+++ client/src/pages/collection/collection.component.jsx
@@ -7,14 +7,12 @@ import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
-	const { title, items } = collection;
-	console.log(collection);
+const CollectionPage = ({ collection: { title, items } }) => {
 	return (
 		<div className="collection-page">
 			<h2 className="title">{title}</h2>
 			<div className="items">
-				{items.map(item => (
+				{items.map((item) => (
 					<CollectionItem key={item.id} item={item} />
 				))}
 			</div>
@@ -23,7 +21,7 @@ const CollectionPage = ({ collection }) => {
 };
 
 const mapStateToProps = (state, ownProps) => ({
-	collection: selectCollection(ownProps.match.params.collectionId)(state)
+	collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
 
 export default connect(mapStateToProps)(CollectionPage);
