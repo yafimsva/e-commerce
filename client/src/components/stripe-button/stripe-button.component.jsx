222d724f81eb4a21090d407b29016977fd4f5329
Yafim Vainilovich <yafimsva@gmail.com>
2020-05-13 19:39:03 -0700

    modified console log

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -19,7 +19,7 @@ const StripeCheckoutButton = ({ price }) => {
 				alert('Payment successful');
 			})
 			.catch((error) => {
-				console.log('Payment error: ', JSON.parse(error));
+				console.log('Payment error: ', error);
 				alert(
 					'There was an issue with your payment. Please make sure you use the provided credit card'
 				);
